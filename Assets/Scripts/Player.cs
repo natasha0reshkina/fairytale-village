e342ae155edde09eeec508ed285f281fa271a38b
natasha0reshkina <80550780+natasha0reshkina@users.noreply.github.com>
2025-01-03 05:42:05 +0300

    Add files via upload

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] private float movingSpeed = 10f;
+    private Rigidbody2D rb;
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+    private void Update()
+    {
+
+        Vector2 inputVector = new Vector2(0, 0);
+        if (Input.GetKey(KeyCode.W))
+        {
+            inputVector.y = 1f;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            inputVector.y = -1f;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            inputVector.x = -1f;
+
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            inputVector.x = 1f;
+        }
+        inputVector = inputVector.normalized;
+        rb.MovePosition(rb.position + inputVector * (movingSpeed * Time.fixedDeltaTime));
+    }
+}
